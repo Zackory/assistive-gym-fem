1e792713779d8c22dd63a2c9d15f8fba7fbc04f0
kpputhuveetil <kpputhuveetil@vcu.edu>
2021-09-16 04:33:51 -0400

    parse known args, require env name

--- assistive_gym/__main__.py
+++ assistive_gym/__main__.py
@@ -3,8 +3,8 @@ from .env_viewer import viewer
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Assistive Gym Environment Viewer')
-    parser.add_argument('--env', default='ScratchItchJaco-v1',
+    parser.add_argument('--env', default='BeddingManipulationSphere-v1', required=True,
                         help='Environment to test (default: ScratchItchJaco-v1)')
-    args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
 
     viewer(args.env)
