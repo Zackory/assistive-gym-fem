445cd93f4dd2dc5a8276d68cd44a7c9e45ea2ca9
kpputhuveetil <kpputhuveetil@vcu.edu>
2022-02-23 23:34:08 -0800

    changed how multiprocessing.Pool is applied fixed seeding

--- assistive_gym/gnn_dc.py
+++ assistive_gym/gnn_dc.py
@@ -10,7 +10,8 @@ def sample_action(env):
 
 def gnn_data_collect(env_name, i):
     coop = 'Human' in env_name
-    env = make_env(env_name, coop=coop, seed=seeding.create_seed())
+    seed = seeding.create_seed()
+    env = make_env(env_name, coop=coop, seed=seed)
 
     done = False
     #env.render()
@@ -21,7 +22,7 @@ def gnn_data_collect(env_name, i):
         # action = np.array([0,0,0,0])
         observation, reward, done, info = env.step(action)
     
-    filename = f"c{i}_{env.seed}_pid{pid}"
+    filename = f"c{i}_{seed}_pid{pid}"
     with open(os.path.join(pkl_loc, filename +".pkl"),"wb") as f:
         pickle.dump({
             "observation":observation, 
@@ -31,8 +32,6 @@ def gnn_data_collect(env_name, i):
     del env
     return output
 
-counter = 0
-
 def counter_callback(output):
     global counter
     counter += 1
@@ -41,15 +40,16 @@ def counter_callback(output):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Assistive Gym Environment Viewer')
-    parser.add_argument('--env', default='ScratchItchJaco-v1',
-                        help='Environment to test (default: ScratchItchJaco-v1)')
+    parser = argparse.ArgumentParser(description='Data collection for gnn training')
+    parser.add_argument('--env', default='BodiesUncoveredGNN-v1')
     args = parser.parse_args()
 
     current_dir = os.getcwd()
-    pkl_loc = os.path.join(current_dir,'gnn_test_dc/pickle544')
+    pkl_loc = os.path.join(current_dir,'gnn_new_data/akhil')
     pathlib.Path(pkl_loc).mkdir(parents=True, exist_ok=True)
 
+    # ! TODO: temporary to prevent messy prints, go back and fix where warning is coming from
+    np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 
     counter = 0
 
@@ -57,21 +57,21 @@ if __name__ == "__main__":
     num_processes = multiprocessing.cpu_count() - 1
 
     # num data points to collect
-    trials = 10000
+    trials = 30000
 
-    # trials = 8
+    # trials = 4
     # num_processes = 4
 
+    counter = 0
     result_objs = []
-    for j in range(math.ceil(trials/num_processes)):
-        with multiprocessing.Pool(processes=num_processes) as pool:
-            for i in range(num_processes):
-                result = pool.apply_async(gnn_data_collect, args = (args.env, i), callback=counter_callback)
-                result_objs.append(result)
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        for i in range(trials):
+            result = pool.apply_async(gnn_data_collect, args = (args.env, i), callback=counter_callback)
+            result_objs.append(result)
 
-            results = [result.get() for result in result_objs]
-            # print(len(results))
-            # print(results)
+        results = [result.get() for result in result_objs]
+    
+    # print(results)
 
     # result_objs = []
     # with multiprocessing.Pool(processes=num_processes) as pool:
