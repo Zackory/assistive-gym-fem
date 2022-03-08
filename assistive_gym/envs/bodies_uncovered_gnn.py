ed3bcb223081c919051467414c0cdbde5e7f6c6c
kpputhuveetil <kpputhuveetil@vcu.edu>
2022-03-07 23:51:22 -0800

    configured to run data collection with high pose variation brought in increase_pose_variation() and using it now take RGB-D image before the blanket drops onto the human specified seperate info dict for data collection capture image to generate point cloud after blanket dropped, before action
    minor typo fix
    initial testing with new scheme for naive approach (UNFINISHED)

--- assistive_gym/envs/bodies_uncovered_gnn.py
+++ assistive_gym/envs/bodies_uncovered_gnn.py
@@ -1,4 +1,4 @@
-import os, time
+import os, time, pickle
 import numpy as np
 import pybullet as p
 
@@ -28,39 +28,101 @@ class BodiesUncoveredGNNEnv(AssistiveEnv):
         self.rendering = False
         self.target_limb_code = 14 if not self.single_ppo_model else None # randomize in reset function, None acts as a placeholder
         self.fixed_pose = False
-        self.collect_data = False
+        self.collect_data = True
         self.blanket_pose_var = False
         self.naive = False
         self.clip = True
 
         self.human_no_occlusion_RGB = None
         self.human_no_occlusion_depth = None
+        self.point_cloud_initial = None
+        self.point_cloud_final = None
+        self.point_cloud_depth_img = None
 
     def set_target_limb_code(self, code):
         self.target_limb_code = code
 
-    def get_naive_action(self, obs):
-        
+    def get_naive_action(self, obs, target_limb_code, data):
+        target_limb_code = 2
         human_pose = np.reshape(obs, (-1,2))
-        feet_midpoint = (human_pose[3] + human_pose[9])/2
-        knee_midpoint = (human_pose[4] + human_pose[10])/2
-        hip_midpoint = (human_pose[5] + human_pose[11])/2
-        btw_upperchest_head_midpoint = (human_pose[12] + human_pose[13])/2
+        all_limb_endpoints = [
+            [0], [0,1], [0,2],
+            [3], [3,4], [3,5],
+            [6], [6,7], [6,8],
+            [9], [9,10], [9,11],
+            [None], [None], [None],
+            [None]
+        ]
+        limb_endpoints = all_limb_endpoints[target_limb_code]
+        if len(limb_endpoints) == 2:
+            p1 = human_pose[limb_endpoints[0]]
+            p2 = human_pose[limb_endpoints[1]]
+            midpoint = (p1 + p2)/2 # grasp location
+            print(p1, p2, midpoint)
 
-        coeff = np.polyfit([feet_midpoint[1], knee_midpoint[1]], [feet_midpoint[0], knee_midpoint[0]], 1)
-        line = np.poly1d(coeff)
-        # release_y = list(btw_upperchest_head_midpoint)[1]
-        release_y = list(human_pose[13])[1]
-        release_x = line(release_y)
+            # coeff = np.polyfit([p1[1], p2[1]], [p1[0], p2[0]], 1)
+            # line = np.poly1d(coeff)
 
-        # self.create_sphere(radius=0.01, mass=0.0, pos = list(feet_midpoint)+[0.9], visual=True, collision=True, rgba=[0, 1, 0, 1])
-        # self.create_sphere(radius=0.01, mass=0.0, pos = list(knee_midpoint)+[0.9], visual=True, collision=True, rgba=[1, 1, 0, 1])
-        # self.create_sphere(radius=0.01, mass=0.0, pos = list(hip_midpoint)+[0.9], visual=True, collision=True, rgba=[0, 0, 0, 1])
-        # self.create_sphere(radius=0.01, mass=0.0, pos = list(btw_upperchest_hip_midpoint)+[0.9], visual=True, collision=True, rgba=[0, 0, 0, 1])
-        # self.create_sphere(radius=0.01, mass=0.0, pos = [release_x, release_y, 0.9], visual=True, collision=True, rgba=[1, 0, 0, 1])
+            # axis_vector = p2-p1
+            # unit_axis_vector = axis_vector/np.linalg.norm(axis_vector)
+            dists = []
+            points = []
+            for i, p3 in enumerate(data[1]):
+                axis_vector = p1-p2
+                if target_limb_code in [3,4,5,9,10,11]: 
+                    vector = axis_vector
+                else:
+                    direction = [-1, 1] if target_limb_code in [6,7,8] else [1, -1]
+                    vector = np.array([axis_vector[1], axis_vector[0]])*np.array(direction)
+                cloth_vector = p3[0:2]-midpoint
+                # cloth_vector = midpoint-p3[0:2] if target_limb_code in [3,4,5,9,10,11] else p3[0:2]-midpoint
+                d = np.linalg.norm(np.cross(vector, cloth_vector))/np.linalg.norm(vector) #! may want to replace midpoint with p1 for lower limbs
 
-        action = np.array(list(feet_midpoint) + [release_x] + [release_y])
-        return action
+                vector = vector/np.linalg.norm(vector)
+                cloth_vector = cloth_vector/np.linalg.norm(cloth_vector)
+                theta = np.arccos(np.clip(np.dot(vector, cloth_vector), -1.0, 1.0))
+                if d < 0.01 and p3[2]>=0.58 and theta < np.pi/2:
+                    # print(theta)
+                    dists.append(np.linalg.norm(midpoint-p3[0:2]))
+                    points.append(p3)
+
+                
+            rgb = [0, 0, 0]
+            p.addUserDebugText(text=str('edge'), textPosition=points[np.argmax(dists)], textColorRGB=rgb, textSize=1, lifeTime=0, physicsClientId=self.id)
+            release = midpoint + (midpoint - points[np.argmax(dists)][0:2])
+            # p.addUserDebugText(text=str('edge'), textPosition=release, textColorRGB=[1,1,0], textSize=1, lifeTime=0, physicsClientId=self.id)
+            self.create_sphere(radius=0.01, mass=0.0, pos = list(release)+[0.9], visual=True, collision=True, rgba=[0, 1, 1, 1])
+    
+
+            self.create_sphere(radius=0.01, mass=0.0, pos = list(p1)+[0.9], visual=True, collision=True, rgba=[0, 1, 0, 1])
+            self.create_sphere(radius=0.01, mass=0.0, pos = list(p2)+[0.9], visual=True, collision=True, rgba=[1, 1, 0, 1])
+            self.create_sphere(radius=0.01, mass=0.0, pos = list(midpoint)+[0.9], visual=True, collision=True, rgba=[0, 0, 0, 1])
+
+            #! CLIP GRASP AND RELEASE TO THE BED
+            
+        else:
+            pass
+        
+        # human_pose = np.reshape(obs, (-1,2))
+        # feet_midpoint = (human_pose[3] + human_pose[9])/2
+        # knee_midpoint = (human_pose[4] + human_pose[10])/2
+        # hip_midpoint = (human_pose[5] + human_pose[11])/2
+        # btw_upperchest_head_midpoint = (human_pose[12] + human_pose[13])/2
+
+        # coeff = np.polyfit([feet_midpoint[1], knee_midpoint[1]], [feet_midpoint[0], knee_midpoint[0]], 1)
+        # line = np.poly1d(coeff)
+        # # release_y = list(btw_upperchest_head_midpoint)[1]
+        # release_y = list(human_pose[13])[1]
+        # release_x = line(release_y)
+
+        # # self.create_sphere(radius=0.01, mass=0.0, pos = list(feet_midpoint)+[0.9], visual=True, collision=True, rgba=[0, 1, 0, 1])
+        # # self.create_sphere(radius=0.01, mass=0.0, pos = list(knee_midpoint)+[0.9], visual=True, collision=True, rgba=[1, 1, 0, 1])
+        # # self.create_sphere(radius=0.01, mass=0.0, pos = list(hip_midpoint)+[0.9], visual=True, collision=True, rgba=[0, 0, 0, 1])
+        # # self.create_sphere(radius=0.01, mass=0.0, pos = list(btw_upperchest_hip_midpoint)+[0.9], visual=True, collision=True, rgba=[0, 0, 0, 1])
+        # # self.create_sphere(radius=0.01, mass=0.0, pos = [release_x, release_y, 0.9], visual=True, collision=True, rgba=[1, 0, 0, 1])
+
+        # action = np.array(list(feet_midpoint) + [release_x] + [release_y])
+        # return action
 
         
     def step(self, action):
@@ -83,13 +145,13 @@ class BodiesUncoveredGNNEnv(AssistiveEnv):
         # * get points on the blanket, initial state of the cloth
         data_i = p.getMeshData(self.blanket, -1, flags=p.MESH_DATA_SIMULATION_MESH, physicsClientId=self.id)
 
+        # print(obs)
+        # self.get_naive_action(obs, 2, data_i)
+        # time.sleep(100)
+
         # * calculate distance between the 2D grasp location and every point on the blanket, anchor points are the 4 points on the blanket closest to the 2D grasp location
-        dist, is_on_cloth = check_grasp_on_cloth(action, data_i[1])
-        # dist = []
-        # for i, v in enumerate(data_i[1]):
-        #     v = np.array(v)
-        #     d = np.linalg.norm(v[0:2] - grasp_loc)
-        #     dist.append(d)
+        dist, is_on_cloth = check_grasp_on_cloth(action, np.array(data_i[1]))
+
         # * if no points on the blanket are within 2.8 cm of the grasp location, exit 
         clipped = False
         if not is_on_cloth:
@@ -100,6 +162,12 @@ class BodiesUncoveredGNNEnv(AssistiveEnv):
                 return obs, 0, False, {} # for data collect
             elif self.clip:
                 return obs, 0, True, {}
+
+        if self.collect_data:
+            filename = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'point_cloud_data.pkl')
+            point_cloud_params = pickle.load(open(filename,'rb'))
+            self.point_cloud_depth_img = self.get_depth_image_for_point_cloud(
+                point_cloud_params['imgW'], point_cloud_params['imgH'], point_cloud_params['viewMat'], point_cloud_params['projMat'])
 
         anchor_idx = np.argpartition(np.array(dist), 4)[:4]
         # for a in anchor_idx:
@@ -115,7 +183,7 @@ class BodiesUncoveredGNNEnv(AssistiveEnv):
         for i in anchor_idx[1:]:
             pos_diff = np.array(data_i[1][i]) - np.array(data_i[1][anchor_idx[0]])
             constraint_ids.append(p.createSoftBodyAnchor(self.blanket, i, self.sphere_ee.body, -1, pos_diff))
-
+        
         # * move sphere up by some delta z
         current_pos = self.sphere_ee.get_base_pos_orient()[0]
         delta_z = 0.4                           # distance to move up (with respect to the top of the bed)
@@ -165,16 +233,25 @@ class BodiesUncoveredGNNEnv(AssistiveEnv):
             all_body_points = get_body_points_from_obs(human_pose, target_limb_code=self.target_limb_code)
             body_point_reward, covered_status = get_body_points_reward(all_body_points, cloth_initial_2D, cloth_final_2D)
             reward = body_point_reward + reward_distance_btw_grasp_release
-        info = {
-            "cloth_initial": data_i,
-            "cloth_final": data_f,
-            "RBG_human": self.human_no_occlusion_RGB,
-            "depth_human": self.human_no_occlusion_depth,
-            "cloth_initial_subsample": cloth_initial_subsample,
-            "cloth_final_subsample": cloth_final_subsample,
-            "covered_status_sim": covered_status,
-            "target_limb_code":self.target_limb_code
-            }
+
+            info = {
+                "cloth_initial": data_i,
+                "cloth_final": data_f,
+                "RBG_human": self.human_no_occlusion_RGB,
+                "depth_human": self.human_no_occlusion_depth,
+                "cloth_initial_subsample": cloth_initial_subsample,
+                "cloth_final_subsample": cloth_final_subsample,
+                "covered_status_sim": covered_status,
+                "target_limb_code":self.target_limb_code
+                }
+        else:
+            info = {
+                "cloth_initial": data_i,
+                "cloth_final": data_f,
+                "RGB_human": self.human_no_occlusion_RGB,
+                "depth_human": self.human_no_occlusion_depth,
+                "point_cloud_depth_img": self.point_cloud_depth_img
+                }
         self.iteration += 1
         done = self.iteration >= 1
 
@@ -233,7 +310,7 @@ class BodiesUncoveredGNNEnv(AssistiveEnv):
             motor_indices, motor_positions, motor_velocities, motor_torques = self.human.get_motor_joint_states()
             # print(motor_positions)
             self.human.set_joint_angles(motor_indices, motor_positions+self.np_random.uniform(-0.2, 0.2, size=len(motor_indices)))
-            # self.increase_pose_variation()
+            self.increase_pose_variation()
             # * Increase friction of joints so human doesn't fail around exessively as they settle
             # print([p.getDynamicsInfo(self.human.body, joint)[1] for joint in self.human.all_joint_indices])
             self.human.set_whole_body_frictions(spinning_friction=2)
@@ -275,13 +352,32 @@ class BodiesUncoveredGNNEnv(AssistiveEnv):
             chair_seat_position = np.array([0, 0.1, 0.55])
             self.human.set_base_pos_orient(chair_seat_position - self.human.get_vertex_positions(self.human.bottom_index), [0, 0, 0, 1])
     
-       
+        # * Setup camera for taking images
+        # *     Currently saves color images only to specified directory
+        if self.take_pictures or self.collect_data:
+            self.setup_camera_rpy(camera_target=[0, 0, 0.305+2.101], distance=0.01, rpy=[0, -90, 180], fov=60, camera_width=468//2, camera_height=398)
+            img, depth = self.get_camera_image_depth()
+            self.human_no_occlusion_RGB = img
+            self.human_no_occlusion_depth = depth
+            depth = (depth - np.amin(depth)) / (np.amax(depth) - np.amin(depth))
+            depth = (depth * 255).astype(np.uint8)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            depth_colormap = cv2.applyColorMap(depth, cv2.COLORMAP_VIRIDIS)
+            # filename = time.strftime("%Y%m%d-%H%M%S") + '.png'
+            # filename = "test_depth.png"
+            # cv2.imwrite(os.path.join('/home/mycroft/git/vBMdev/pose_variation_images/lower_var2', filename), img)
+            # cv2.imwrite(filename, depth_colormap)
+
+
+
         # * spawn blanket
         self.blanket = p.loadSoftBody(os.path.join(self.directory, 'clothing', 'blanket_2089v.obj'), scale=0.75, mass=0.15, useBendingSprings=1, useMassSpring=1, springElasticStiffness=1, springDampingStiffness=0.0005, springDampingAllDirections=1, springBendingStiffness=0, useSelfCollision=1, collisionMargin=0.006, frictionCoeff=0.5, useFaceContact=1, physicsClientId=self.id)
         # * change alpha value so that it is a little more translucent, easier to see the relationship the human
         p.changeVisualShape(self.blanket, -1, rgbaColor=[0, 0, 1, 1], flags=0, physicsClientId=self.id)
         p.changeVisualShape(self.blanket, -1, flags=p.VISUAL_SHAPE_DOUBLE_SIDED, physicsClientId=self.id)
         p.setPhysicsEngineParameter(numSubSteps=4, numSolverIterations = 4, physicsClientId=self.id)
+
+
         if self.blanket_pose_var:
             delta_y = self.np_random.uniform(-0.05, 0.05)
             delta_x = self.np_random.uniform(-0.02, 0.02)
@@ -297,9 +393,6 @@ class BodiesUncoveredGNNEnv(AssistiveEnv):
         for _ in range(100):
             p.stepSimulation(physicsClientId=self.id)
 
-
-        # self.get_point_cloud()
-
         # time.sleep(100)
 
 
@@ -313,7 +406,8 @@ class BodiesUncoveredGNNEnv(AssistiveEnv):
         # self.time = time.time()
         if self.robot is None:
             # * spawn sphere manipulator
-            position = np.array([-0.3, -0.86, 0.8])
+            # position = np.array([-0.3, -0.86, 0.8])
+            position = np.array([2 ,2, 0]) # move out of the way so it doesn't interfere with the initial depth image
             self.sphere_ee = self.create_sphere(radius=0.01, mass=0.0, pos = position, visual=True, collision=True, rgba=[0, 0, 0, 1])
             
             # * initialize env variables
@@ -335,21 +429,16 @@ class BodiesUncoveredGNNEnv(AssistiveEnv):
         else:
             self.init_env_variables()
         
-        
-        # * Setup camera for taking images
-        # *     Currently saves color images only to specified directory
-        if self.take_pictures or self.collect_data:
-            self.setup_camera_rpy(camera_target=[0, 0, 0.305+2.101], distance=0.01, rpy=[0, -90, 180], fov=60, camera_width=468//2, camera_height=398)
-            img, depth = self.get_camera_image_depth()
-            self.human_no_occlusion_RGB = img
-            self.human_no_occlusion_depth = depth
-            depth = (depth - np.amin(depth)) / (np.amax(depth) - np.amin(depth))
-            depth = (depth * 255).astype(np.uint8)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            depth_colormap = cv2.applyColorMap(depth, cv2.COLORMAP_VIRIDIS)
-            # filename = time.strftime("%Y%m%d-%H%M%S") + '.png'
-            # filename = "test_depth.png"
-            # cv2.imwrite(os.path.join('/home/mycroft/git/vBMdev/pose_variation_images/lower_var2', filename), img)
-            # cv2.imwrite(filename, depth_colormap)
 
         return self._get_obs()
+    
+    def increase_pose_variation(self):
+        '''
+        Allow more variation in the knee and elbow angles
+          can be some random position within the lower and upper limits of the joint movement (range is made a little smaller than the limits of the joint to prevent angles that are too extreme)
+        '''
+        for joint in (self.human.j_left_knee, self.human.j_right_knee, self.human.j_left_elbow, self.human.j_right_elbow):
+            motor_indices, motor_positions, motor_velocities, motor_torques = self.human.get_motor_joint_states([joint])
+            self.human.set_joint_angles(motor_indices, motor_positions+self.np_random.uniform(self.human.lower_limits[joint]+0.1, self.human.upper_limits[joint]-0.1, 1))
+
+    
